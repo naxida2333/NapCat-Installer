9176e817174f6c7fef7c653e114d6119d26050a6
naxida233 <naxida233@users.noreply.github.com>
2026-09-22 05:31:49 +0000

    fix: 修正 arm64 下载链接 owner 拼写 (naxida233 -> naxida2333)

--- script/install.sh
+++ script/install.sh
@@ -645,7 +645,7 @@ function install_linuxqq_rootless() {
             qq_download_url="https://qqdl.gtimg.cn/qqfile/QQNT/9.9.32/beta/727ce4e5/linuxqq_3.2.30-50828_aarch64.rpm"
             qq_package_file="QQ.rpm"
         elif [ "${package_installer}" = "dpkg" ]; then
-            qq_download_url="https://github.com/naxida233/QQ/releases/download/V1.0/QQ_3.2.32_260812_arm64_01.deb"
+            qq_download_url="https://github.com/naxida2333/QQ/releases/download/V1.0/QQ_3.2.32_260812_arm64_01.deb"
             qq_package_file="QQ.deb"
         fi
     fi
